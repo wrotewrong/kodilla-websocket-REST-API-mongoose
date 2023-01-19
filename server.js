738c20f56ffaf9e2a9f320fc1788d1591b7a5cd8
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-01-19 17:37:35 +0100

    Deploy the build second time

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const app = express();
 // const { v4: uuidv4 } = require('uuid');
 // const db = require('./db');
 const cors = require('cors');
+const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
-app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use(cors());
 app.use(express.urlencoded({ extends: false }));
 app.use(express.json());
 
@@ -15,11 +19,15 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
